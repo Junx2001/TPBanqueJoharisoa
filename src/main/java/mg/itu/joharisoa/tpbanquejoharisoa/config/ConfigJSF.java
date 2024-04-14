2da9dd0791369fb0c62c3a2e357edc54f067d17a
Junx2001 <ratsirarsonj@gmail.com>
2024-04-14 22:54:01 +0300

    Écriture des pages JSF et des backing beans associés

---
+++ src/main/java/mg/itu/joharisoa/tpbanquejoharisoa/config/ConfigJSF.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.joharisoa.tpbanquejoharisoa.config;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.faces.annotation.FacesConfig;
+
+/**
+ * Configuration JSF
+ * @author xxxx
+ */
+@ApplicationScoped
+@FacesConfig
+public class ConfigJSF {
+}
---
+++ src/main/java/mg/itu/joharisoa/tpbanquejoharisoa/jsf/ListeComptes.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package mg.itu.joharisoa.tpbanquejoharisoa.jsf;
+
+import jakarta.inject.Named;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import java.io.Serializable;
+import java.util.List;
+import mg.itu.joharisoa.tpbanquejoharisoa.entity.CompteBancaire;
+import mg.itu.joharisoa.tpbanquejoharisoa.service.GestionnaireCompte;
+
+/**
+ *
+ * @author ratsi
+ */
+@Named(value = "listeComptes")
+@ViewScoped
+public class ListeComptes implements Serializable {
+
+    private List<CompteBancaire> compteList;
+
+    @Inject
+    private GestionnaireCompte gestionnaireCompte;
+
+    /**
+     * Creates a new instance of ListeComptes
+     */
+    public ListeComptes() {
+    }
+
+    /**
+     * Retourne la liste des Comptes pour affichage dans une DataTable.
+     *
+     * @return List
+     */
+    public List<CompteBancaire> getAllComptes() {
+        if (compteList == null) {
+            compteList = gestionnaireCompte.getAllComptes();
+        }
+        return compteList;
+    }
+
+}
