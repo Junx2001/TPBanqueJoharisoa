85ce1b35b074dd8b81a5d385320d53d095c8edc3
Junx2001 <ratsirarsonj@gmail.com>
2024-04-14 20:11:58 +0300

    Déclaration de la source de données

--- src/main/java/mg/itu/joharisoa/tpbanquejoharisoa/service/GestionnaireCompte.java
+++ src/main/java/mg/itu/joharisoa/tpbanquejoharisoa/service/GestionnaireCompte.java
@@ -1,5 +1,6 @@
 package mg.itu.joharisoa.tpbanquejoharisoa.service;
 
+import jakarta.annotation.sql.DataSourceDefinition;
 import mg.itu.joharisoa.tpbanquejoharisoa.entity.CompteBancaire;
 import java.util.List;
 import jakarta.enterprise.context.RequestScoped;
@@ -12,6 +13,20 @@ import jakarta.transaction.Transactional;
  * Gère la persistance des Comptes Bancaire.
  */
 @RequestScoped
+@DataSourceDefinition (
+    className="com.mysql.cj.jdbc.MysqlDataSource",
+    name="java:app/jdbc/banque",
+    serverName="localhost",
+    portNumber=3306,
+    user="root",              // nom et
+    password="root", // mot de passe que vous avez donnés lors de la création de la base de données
+    databaseName="banque",
+    properties = {
+      "useSSL=false",
+      "allowPublicKeyRetrieval=true",
+      "driverClass=com.mysql.cj.jdbc.Driver"
+    }
+)
 public class GestionnaireCompte {
 
     @PersistenceContext(unitName = "compteBancairePU")
