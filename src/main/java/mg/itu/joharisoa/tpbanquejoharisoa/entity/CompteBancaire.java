cb9fa58db4ceef72d397f2d18eda8150f1cc0b27
Junx2001 <ratsirarsonj@gmail.com>
2024-04-14 19:49:38 +0300

    Écriture d'une entité CompteBancaire

---
+++ src/main/java/mg/itu/joharisoa/tpbanquejoharisoa/entity/CompteBancaire.java
@@ -0,0 +1,113 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mg.itu.joharisoa.tpbanquejoharisoa.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.io.Serializable;
+
+/**
+ *
+ * @author ratsi
+ */
+@Entity
+public class CompteBancaire implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nom;
+    private int solde;
+
+    public CompteBancaire() {
+    }
+    
+   
+    public CompteBancaire(String nom, int solde) {
+        this.nom = nom;
+        this.solde = solde;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * Get the value of nom
+     *
+     * @return the value of nom
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Set the value of nom
+     *
+     * @param nom new value of nom
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * Get the value of solde
+     *
+     * @return the value of solde
+     */
+    public int getSolde() {
+        return solde;
+    }
+
+    /**
+     * Set the value of solde
+     *
+     * @param solde new value of solde
+     */
+    public void setSolde(int solde) {
+        this.solde = solde;
+    }
+
+    public void deposer(int montant) {
+        solde += montant;
+    }
+
+    public void retirer(int montant) {
+        if (montant < solde) {
+            solde -= montant;
+        } else {
+            solde = 0;
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof CompteBancaire)) {
+            return false;
+        }
+        CompteBancaire other = (CompteBancaire) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mg.itu.joharisoa.tpbanquejoharisoa.entity.CompteBancaire[ id=" + id + " ]";
+    }
+
+}
