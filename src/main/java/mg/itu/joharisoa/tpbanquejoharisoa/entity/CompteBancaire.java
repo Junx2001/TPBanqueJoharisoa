50710de85ca4e1c34278e49cb58c97565f9f2d4f
Junx2001 <ratsirarsonj@gmail.com>
2024-04-18 14:59:33 +0300

    TP 5 - N+1 Select Resolve

--- src/main/java/mg/itu/joharisoa/tpbanquejoharisoa/entity/CompteBancaire.java
+++ src/main/java/mg/itu/joharisoa/tpbanquejoharisoa/entity/CompteBancaire.java
@@ -23,7 +23,7 @@ import java.util.List;
  */
 @Entity
 @NamedQueries({
-    @NamedQuery(name = "CompteBancaire.findAll", query = "SELECT c FROM CompteBancaire c"),
+    @NamedQuery(name = "CompteBancaire.findAll", query = "SELECT DISTINCT c FROM CompteBancaire c join fetch c.operations"),
     @NamedQuery(name = "CompteBancaire.count", query = "SELECT count(c) from CompteBancaire c")
 })
 public class CompteBancaire implements Serializable {
